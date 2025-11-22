4887525781303c2d1746585838a87cf7a38e6d27
Un Carlos <carlosj.pineda07@gmail.com>
2025-11-21 22:28:59 -0400

    fix: corregir error de input numérico en de formulario

--- src/assets/js/helpers/regexp.js
+++ src/assets/js/helpers/regexp.js
@@ -1,14 +1,14 @@
 export const regExp = {
 
     // Expresiones regulares para validaciones
-    texto:/^[0-9A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,15}$/,
-    nombreApellido: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,15}$/, 
-    cantidad: /^[0-9]{2,11}$/,               
-    telefono: /^[0-9]{10}$/,                
+    texto: /^[0-9A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,15}$/,
+    nombreApellido: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,15}$/,
+    cantidad: /^[0-9]{2,11}$/,
+    telefono: /^[0-9]{10}$/,
     email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
     cedula: /^[0-9]{7,15}$/,
-    hoy: new Date().toISOString().split("T")[0], 
+    hoy: new Date().toISOString().split("T")[0],
     descripcion: /^.{2,100}$/,
-    precio: /^[0-9]+(\.[0-9]{1,2})?$/, // Permite decimales con hasta 2 decimales
-    stock: /^[0-9]+$/, // Solo números enteros positivos
+    precio: /^[0-9]{1,8}(\.[0-9]{1,2})?$/, // Permite decimales con hasta 2 decimales
+    stock: /^[0-9]{1,10}$/, // Solo números enteros positivos
 }
